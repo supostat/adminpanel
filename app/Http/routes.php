af2d5af66bfddbfa41eb2c6e11df2851061b3e07
supostat <ipugachev84@gmail.com>
2015-04-01 01:18:00 +0600

    Whoops install and configure

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('error', function(){
+	throw new \Exception;
+});
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
